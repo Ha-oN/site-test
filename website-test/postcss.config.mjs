0d9ab704d53714dc8dde2844054227861f82ac78
Noah <forumentreprises@centrale-marseille.fr>
2026-03-09 17:46:24 +0100

    functioning main page!

--- website-test/postcss.config.mjs
+++ website-test/postcss.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {}, // Change 'tailwindcss' to '@tailwindcss/postcss'
   },
 };
 
